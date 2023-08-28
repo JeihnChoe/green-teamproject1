8252a231eefd5f12c255d06fa15eefae2e10c77d
JeihnChoe <baronjae@gmail.com>
2023-08-28 14:51:33 +0900

    최초화면세팅시작

---
+++ src/test/java/shop/mtcoding/teamprojectgroup2/TeamprojectGroup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.teamprojectgroup2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprojectGroup2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
