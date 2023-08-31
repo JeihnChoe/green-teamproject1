76b17c106e20081abc44f2f8026136bf455a45c6
JeihnChoe <baronjae@gmail.com>
2023-08-31 14:22:11 +0900

    [fix]더미데이터오류수정완료

--- src/main/java/shop/mtcoding/teamprojectgroup2/relationEntities/Apply.java
+++ src/main/java/shop/mtcoding/teamprojectgroup2/relationEntities/Apply.java
@@ -1,19 +1,33 @@
 package shop.mtcoding.teamprojectgroup2.relationEntities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Data
 @Entity
 @Table(name = "apply_tb")
 public class Apply {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+
+    @Column
     Integer resumeId;
+
+    @Column
     Integer noticeId;
+
+    @Column
     String pass;
 
     @Builder
